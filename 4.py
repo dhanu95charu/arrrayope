1cc750532b4b69d7f82ca183510202a2047be9fa
charan <charan@gmail.com>
2023-08-17 11:59:42 +0530

    change4

---
+++ 4.py
@@ -0,0 +1,16 @@
+import numpy as np
+b=np.array((1,3,2))
+print("array plus one",b+1)
+b=np.array((1,3,2))
+print("array sub one",b-1)
+b=np.array((1,3,2))
+print("array mul one",b*1)
+b=np.array((1,3,2))
+print("array cuberoot one",b**2)
+b=np.array((1,3,2))
+print("array mod one",b%2)
+b=np.array((1,3,2))
+print("array div one",b/2)
+b=np.array((1,3,2))
+print("array operand one",b& 1)
+
